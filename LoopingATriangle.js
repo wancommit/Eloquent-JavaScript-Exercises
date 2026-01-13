66bbce35cb01ca86d4d7875f50b6adc110be09ff
Kelvin Bariche <kelvinbariche@Kelvins-MacBook-Pro.local>
2026-01-13 07:34:02 +0000

    exercise solution

---
+++ LoopingATriangle.js
@@ -0,0 +1,11 @@
+// Write a loop that makes seven calls to console.log to out the following triangle
+// #
+//##
+//###
+//####
+//#####
+//######
+//#######
+
+for (let line = "#"; line.length < 8; line += "#");
+    console.log(line)
